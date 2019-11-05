8bff42999703ebe433289378271dcdb5b97b358f
gzl <gaozheling@pku.edu.cn>
2019-11-05 15:34:35 +0800

    3-使用DevUI组件库

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DevUIModule } from 'ng-devui';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DevUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
